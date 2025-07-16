4bcf749285a6702a110f23bfd1bb43f7af795e1d
gab-braga <gabrielbraga0712@gmail.com>
2025-07-16 11:00:59 -0300

    evento com a lampada

--- JS/aula-16/lampada/script.js
+++ JS/aula-16/lampada/script.js
@@ -1,17 +1,35 @@
-const lampada = document.querySelector("#lampada");
+const lampada = document.querySelector("#lampada"); // img (lâmpada)
+const botao = document.querySelector("#botao"); // button (botão)
+const chave = document.querySelector("#chave"); // img (interruptor)
 
 const lampadaAcesa = "./images/lampada-acesa.png";
 const lampadaApaga = "./images/lampada-apagada.png";
 
+const on = "./images/on.png";
+const off = "./images/off.png";
+
 let apagada = true;
 
-setInterval(() => {
+botao.addEventListener("click", () => {
     if (apagada) {
+        chave.src = on;
         lampada.src = lampadaAcesa;
         document.body.style.backgroundColor = "white";
     } else {
+        chave.src = off;
         lampada.src = lampadaApaga;
         document.body.style.backgroundColor = "#181818";
     }
     apagada = !apagada;
-}, 2000);
+});
+
+// setInterval(() => {
+//     if (apagada) {
+//         lampada.src = lampadaAcesa;
+//         document.body.style.backgroundColor = "white";
+//     } else {
+//         lampada.src = lampadaApaga;
+//         document.body.style.backgroundColor = "#181818";
+//     }
+//     apagada = !apagada;
+// }, 2000);
